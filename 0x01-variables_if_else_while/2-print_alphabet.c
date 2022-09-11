0a23ba20b15a4da52c17693e9f48481f8cbcc29c
Your Name <you@example.com>
2022-09-11 10:48:06 -0700

    test file back again

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char start = 'a';
+char end = 'z';
+char ch = start;
+while (ch != end)
+{
+putchar(ch);
+ch++;
+}
+
+return (0);
+}
