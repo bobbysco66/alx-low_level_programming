b1a82f5e26ed0448b76bf37c8a74a10d51e8d02e
Your Name <you@example.com>
2022-09-11 07:52:00 -0700

    test file back again

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Use 'prinf' to output a string to stdout
+ *
+ * Return: Returns 0 if output string to stdout with no issue
+ */
+
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
