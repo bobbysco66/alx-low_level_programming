ec1c0861491ce578dbeb309e0144e56400f38e22
Your Name <you@example.com>
2022-09-11 18:27:13 -0700

    test file back again

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char start = 'z';
+char end = 'a';
+char ch = start;
+while (end <= ch)
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
