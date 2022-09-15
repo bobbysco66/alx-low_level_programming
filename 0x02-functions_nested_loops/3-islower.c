7b33ffed7f4c197af129d328bd77a278850b3e38
Your Name <you@example.com>
2022-09-14 17:49:09 -0700

    test file back again

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,8 +5,10 @@
  *@c:  is the int that will use for the argument of the function
  * Return: Always 0 (Success)
  */
-int _islower(int c);
-if (ch >= 'a' && ch <= 'z')
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
 {return (1); }
 else
 {return (0); }
+}
