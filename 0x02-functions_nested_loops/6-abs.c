aa503cf46898ddf698c639a8e73fd4f8afc494a2
Your Name <you@example.com>
2022-09-14 18:16:25 -0700

    test file back again

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - Entry point
+ *@c:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+int _abs(int c)
+{
+if (c >= 0)
+{return (c);
+}
+else
+{return (c * -1);
+}
+}
