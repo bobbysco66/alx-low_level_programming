9036d125a49a709ac1c3bba7bb94dc920562fb86
Your Name <you@example.com>
2022-09-14 18:35:24 -0700

    test file back again

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_last_digit - Entry point
+ *@c:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+return (c % 10);
+}
