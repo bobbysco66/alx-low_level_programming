7599ab65472076ab4072ba0099ee464845b200a6
Your Name <you@example.com>
2022-09-16 15:46:55 -0700

    test file back again

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagonal - Entry point
+ *@n: number of times
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i;
+int gap;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (gap = 0; gap < i; gap++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
