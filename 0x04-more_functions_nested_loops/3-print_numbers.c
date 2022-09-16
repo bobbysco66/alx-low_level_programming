257e1f39b7093040ef9860472a39e3b6afad3edf
Your Name <you@example.com>
2022-09-16 14:16:51 -0700

    test file back again

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,8 @@
  */
 void print_numbers(void)
 {
-int n;
- for (n = 48; n <= 58; n++)
-{_putchar(n);
-}
-_putchar(10);
+int num;
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
+_putchar('\n');
 }
