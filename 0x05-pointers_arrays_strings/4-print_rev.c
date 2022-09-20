1fa75173e210edeb77e3aa9814854f2411740eb1
Your Name <you@example.com>
2022-09-19 19:44:39 -0700

    test file back again

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_rev - Entry point
+ *@s:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+int c = 0;
+while (*s != '\0')
+{
+c++;
+s++;
+}
+for (c >= 0)
+{
+_putchar(s[c]);
+c--;
+}
+_putchar('\n');
+}
