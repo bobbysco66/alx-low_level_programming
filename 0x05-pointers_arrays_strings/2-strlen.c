688ca78d6866fe4b1cdfe883ffb9898931d8be19
Your Name <you@example.com>
2022-09-19 18:21:21 -0700

    test file back again

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - Entry point
+ *@s:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int c = 0;
+while (*s != '\0')
+{
+c++;
+*s++;
+}
+return (c);
+}
