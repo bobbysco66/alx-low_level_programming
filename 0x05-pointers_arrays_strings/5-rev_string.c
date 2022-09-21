07f93af20c14f23e6a6074e0bb3a355341914c3d
Your Name <you@example.com>
2022-09-20 17:35:52 -0700

    test file back again

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *rev_string - Entry point
+ *@s:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+rev_string(char *s);
+{
+int c = 0;
+char *temp = *s;
+int len;
+int a = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+len = c; 
+for (a <= len)
+{
+s[a] = temp[c];
+a++;
+c--;
+}
+}
