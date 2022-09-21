6fa277f4b79a3841da78aa10fa9c386da156558e
Your Name <you@example.com>
2022-09-20 18:59:21 -0700

    test file back again

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - Entry point
+ *@a:  is the int that will use for the argument of the function
+ *@n: another int
+ *Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+int c;
+for (c = 0; c < n; c++)
+{
+printf("%d", a[c]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+putchar('\n');
+}
