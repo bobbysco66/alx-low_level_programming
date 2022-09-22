783f6892007abdf892ed2dd111a64f0354a667ac
Your Name <you@example.com>
2022-09-22 15:43:20 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - Entry point
+ *@s1:  is the int that will use for the argument of the function
+ *@s2: another one
+ *Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+int a = 0;
+int b = 0;
+while (s1[a] != '\0' && b == 0)
+{
+b = s1[a] - s2[a];
+a++;
+}
+if (b > 0)
+{
+return (15);
+}
+else if (b < 0)
+{
+return (-15);
+}
+ else
+{
+return (0);
+}
+}
