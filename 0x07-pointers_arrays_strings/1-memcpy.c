2cb5e745fec8d1e08beb12d73647328640e066aa
Your Name <you@example.com>
2022-09-27 13:35:17 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - fills memory with constant byte
+ * @dest: pointer to the memory area
+ * @src: bytes of memory
+ * @n: constant byte
+ *
+ * Return: the memory area of s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest)
+}
