ae0eaa0c95bc939da9d5bd2788c4905925f1aec0
Your Name <you@example.com>
2022-09-30 10:33:33 -0700

    create static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - Entry point
+ *@s:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+void _puts(char *s)
+{
+  while (*s != '\0')
+    {
+      _putchar(*s);
+      s++;
+    }
+  _putchar('\n');
+}
