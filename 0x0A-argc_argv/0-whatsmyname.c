5d552ec3e58781ae02899f3d846468e2800bcd1c
Your Name <you@example.com>
2022-10-01 19:50:11 -0700

    arg and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: the strings in an array form
  *Return: On success 1.
  */
-int main(__attribute__((unused)) int argc, char *argv[]) 
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
