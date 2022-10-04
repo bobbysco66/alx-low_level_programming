2ff22ecda008c47a3056cfe9ba622aa290ab6c57
Your Name <you@example.com>
2022-10-03 17:55:32 -0700

    arg and argv - rewritten because of issues

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,22 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-int tot = 0;
-if (argc == 1)
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
 {
-printf("%d\n", 0);
-return (0);
-}
-for (i = 1; i < argc ; i++)
+for (digit = 0; argv[num][digit]; digit++)
 {
-tot = tot + atoi(argv[i]);
-if (atoi(argv[i]) == 0)
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
 {
 printf("Error\n");
 return (1);
 }
 }
-printf("%d\n", tot);
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
 return (0);
 }
