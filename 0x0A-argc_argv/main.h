f25c5bc92a969e236735a12219fa857cc1cd7686
Your Name <you@example.com>
2022-10-03 17:17:34 -0700

    arg and argv

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+int main(int argc, char *argv[]);
 int _putchar(char c);
 int main(__attribute__((unused)) int argc, char *argv[]) ;
 #endif /* MAIN_H */
