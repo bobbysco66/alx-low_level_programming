980f45dcc3e4a4f972d1ef2f31643e449249d6d5
Your Name <you@example.com>
2022-10-04 17:18:49 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - create an array of chars
+ * @str: size of the memory to print
+ *
+ * Return: a pointer of array or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int i;
+int m = 0;
+if (str == NULL)
+{
+return (NULL);
+}
+i = strlen(str) + 1;
+dup = malloc(strlen(str) + 1);
+while (m <= i)
+{
+dup[m] = str[m];
+m++;
+}
+return (dup);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,6 @@
 #include <stdio.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
