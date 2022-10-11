82b5d63e4e8c72d2bbd09fb77818b62e2f56de07
Your Name <you@example.com>
2022-10-10 19:34:29 -0700

    this is for structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,13 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
