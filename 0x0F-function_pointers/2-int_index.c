fced647e974adccf400df046402779676b9842c3
Your Name <you@example.com>
2022-10-12 17:30:37 -0700

    pointers of functions is interesting

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - search for index
+ * @size: The size
+ * @array: The array
+ * @cmp: The name
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+{
+return (-1);
+}
+if (array != NULL && cmp != NULL)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i); 
+}
+}
+}
+return (-1);  
+}
