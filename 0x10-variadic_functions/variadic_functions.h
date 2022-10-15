b1245bb5008a629040475ddd8ed2ed84ed561963
Your Name <you@example.com>
2022-10-14 18:44:36 -0700

    review this

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+typedef struct print_all
+{
+  char all;
+  void (*func)(va_list arguments);
+} simbol_t;
+
 #endif /* MAIN_H */
