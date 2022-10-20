2c4c703fa8894b6a93002bf6cdd244b7a169020c
Your Name <you@example.com>
2022-10-20 11:34:06 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *_strlen - Entry point
+ *@s:  is the int that will use for the argument of the function
+ * Return: Always 0 (Success)
+ */
+int _strlen(const char *s)
+{
+int c = 0;
+while (*s != '\0')
+{
+c++;
+s++;
+}
+return (c);
+}
+/**
+ * add_node - add node to the beginning a list
+ * @head: points to the header node
+ * @str: the string to add
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->str = strdup(str);
+new->len = _strlen(str);
+/* make next the new node as head*/
+new->next = *head;
+/*move the head to point at the new node*/
+*head = new;
+return (*head);
+}
