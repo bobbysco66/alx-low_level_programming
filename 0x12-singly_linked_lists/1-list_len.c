63e584e91fcaf59d4b9b003e63c63d4e755e136d
Your Name <you@example.com>
2022-10-20 09:37:08 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - print all the elements of a list
+ * @h: points to the header node
+ * Return: unsigned long int that shows total number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+size_t nodes = 0;
+while ( h != NULL)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
+}
