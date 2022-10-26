11a814902eabc7671bf8cb6dab0f1387f8481e53
Your Name <you@example.com>
2022-10-25 17:00:10 -0700

    singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - this function frees a list
+ * @head: the pointer that points to the struct
+ * Return: the adress of the new element
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *p, *y;
+p = *head;
+while (p != NULL)
+{
+y = p;
+p = p->next;
+free(y);
+}
+*head = NULL;
+}
